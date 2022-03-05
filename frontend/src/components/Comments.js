8688b0f43f2ff1046503d943472976ac1f345543
hassanz93 <hassanzahra911@gmail.com>
2022-03-05 18:48:51 +0200

    fixed Comments and made them static

--- frontend/src/components/Comments.js
+++ frontend/src/components/Comments.js
@@ -1,41 +1,49 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import '../css/Comments.css';
-import Backend from "../services/Backend";
+
 
 
 function TraineeComments() {
-    const [comments, setComments] = useState([]);
 
-    useEffect(() => {
-        retrieveComments()
-    }, []);
 
-    const retrieveComments = () => {
-        Backend.getComments()
-            .then((response) => {
-                setComments(response.data)
-                console.log(response.data)
-            }).catch((error) => {
-                console.log(error);
-            });
-    }
 
     const displayData = () => {
-        return (
-            comments.map((comment) => {
-                return (
-                    <div className="comment-box"  >
-                        <div className="comment--collectionpictures">
-                            <img className="comment--picture" src={comment.receiver.photo} alt={"photo not available"} />
-                            <img className="comment--sidepicture" src="https://www.dummies.com/wp-content/uploads/494382.image1.jpg" alt={"photo not available"} />
-                        </div>
-                        <p className="comment--linedivider"> _________________________________________ </p>
-                        <p className="comment--commentname"> {comment.receiver.fname + " " + comment.receiver.lname} - <strong className="italic"> Top Student</strong> </p>
-                        <p className="comment--message"> {comment.message} </p>
-                    </div>
 
-                )
-            })
+
+        return (
+            <>
+            <div className="comment-box"  >
+                <div className="comment--collectionpictures">
+                    <img className="comment--picture" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrJZTUP6x0wf971Bz8Y2Y0nrllmqbhqtRp4w&usqp=CAU" alt={"photo not available"} />
+                    <img className="comment--sidepicture" src="https://www.dummies.com/wp-content/uploads/494382.image1.jpg" alt={"photo not available"} />
+                </div>
+                <p className="comment--linedivider"> _________________________________________ </p>
+                <p className="comment--commentname"> Ludris Valery - <strong className="italic"> Music Student</strong> </p>
+                <p className="comment--message"> My instructor very good at assessing a student’s abilities and interests and adjusting his lessons accordingly. He makes the lessons fun and challenging. </p>
+            </div>
+
+            <div className="comment-box"  >
+                <div className="comment--collectionpictures">
+                    <img className="comment--picture" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrJZTUP6x0wf971Bz8Y2Y0nrllmqbhqtRp4w&usqp=CAU" alt={"photo not available"} />
+                    <img className="comment--sidepicture" src="https://www.dummies.com/wp-content/uploads/494382.image1.jpg" alt={"photo not available"} />
+                </div>
+                <p className="comment--linedivider"> _________________________________________ </p>
+                <p className="comment--commentname">  Sarra Merob - <strong className="italic"> Martial Arts Student</strong> </p>
+                <p className="comment--message"> Absolutely wonderful experience. The best Sensei to go for Martial Arts. Sensei Best is wonderful with working with all ages. Great energy and sense of humor. Very supportive environment. Hoping to join back again soon! </p>
+            </div>
+
+            <div className="comment-box"  >
+                <div className="comment--collectionpictures">
+                    <img className="comment--picture" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrJZTUP6x0wf971Bz8Y2Y0nrllmqbhqtRp4w&usqp=CAU" alt={"photo not available"} />
+                    <img className="comment--sidepicture" src="https://www.dummies.com/wp-content/uploads/494382.image1.jpg" alt={"photo not available"} />
+                </div>
+                <p className="comment--linedivider"> _________________________________________ </p>
+                <p className="comment--commentname"> Merlyn Kliment  - <strong className="italic"> Sports Student</strong> </p>
+                <p className="comment--message"> Alan Best has had a tremendous amount of experience. He works not only as an instructor, but as a mentor. I have watched the affect he has on personal growth with his students, and it is an intangible skill which stars cannot account for.  </p>
+            </div>
+
+            </>
+
         )
     }
     return (<div className="comment-container">
