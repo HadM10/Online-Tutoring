f8a077fa2194aad5d84c090d9fbfce9c734236bf
HadM10 <hadmak10@gmail.com>
2022-03-20 10:23:00 +0200

    all

--- frontend/src/components/TrainerSide.js
+++ frontend/src/components/TrainerSide.js
@@ -4,6 +4,7 @@ import axios from "../axios";
 import Backend from "../services/Backend";
 import { Link, useParams } from 'react-router-dom'
 import '../css/TrainerSide.css'
+import TrainerTutorials from "./TrainerTutorials";
 
 
 function TrainerSide() {
@@ -41,6 +42,10 @@ else{
 }
 }
 
+const inputDateTime = (e) =>{
+    setDate_time(e)
+} 
+
     const retrieveTutorials = () => {
         Backend.getTrainerTutorials()
             .then((response) => {
@@ -51,43 +56,13 @@ else{
 
             });
     }
+
     const displayTutorials = () => {
         return (
-            tutorials.map((Tutorial) => {
-                return (
-                    <>
-                        <div class="card">
-                            {console.log(Tutorial)}
-                            <img src={Tutorial.photo} alt="category" className="tutorial-image"></img>
-                            {/* <img src={Tutorial.trainerId.photo} alt="category" className="tutorial-imagesmall"></img> */}
-
-                            <div className="tutorial-titles">
-                                <div className="tutorial-name"><h3 className="tutorial-title">{Tutorial.subCategories.name}</h3></div>
-                                <div className="tutorial-price"><h3 className="tutorial-title">{Tutorial.pricePerLesson}/hr</h3></div>
-                            </div>
-                            <p className='tutorial-maintitle'>{Tutorial.title}</p>
-                            <div className="description-tuto">
-                            <p className='tutorial-description'>{Tutorial.description}</p>
-                            </div>
-                            <div className="datetime-settings">
-                            <ul className="all-the-datetimes">{Tutorial.dateTime.map((dayData, i) => {return dayData.available === true? <li key={i} value={dayData.DateTime}> {dayData.DateTime}</li> : '' })}</ul>
-                            <br></br>
-                            <ul className="all-the-datetimes"><span style={{}}>MY SCHEDULE</span> {Tutorial.dateTime.map((dayData, i) => {return dayData.available === false? <li key={i} value={dayData.DateTime}> {dayData.DateTime}</li> : '' })}</ul>
-                            <br></br>
-                            
-                            <form className="add-new-datetime">
-                            <input type="text" className="register-Input-phone  datetime-input" name="DateTime" onChange={(e) => setDate_time(e.target.value)} placeholder='Add a new DateTime' value={date_Time}/>
-                            <button className='add-datetime' type='button' value="Submit" onClick={() => handleSubmit(Tutorial._id)}/>
-                            </form>
-                            </div>
-                            <Link className='link-categories' to={`lessons/${Tutorial._id}`}>
-                                <button className='tutorial-button'>More</button>
-                            </Link>
-
-                        </div>
-                    </>
-                )
-            })
+            tutorials.map((Tutorial) => (
+               <TrainerTutorials Tutorial={Tutorial}/>
+                
+            ))
         )
     }
     return (
